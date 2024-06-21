af12d1ce2234180d837d83d9306f509dc5f584d3
Marko <markogidej@yahoo.com>
2024-06-21 12:14:20 -0400

    Q2 all tasks done

--- stringManipulation.js
+++ stringManipulation.js
@@ -1,10 +1,15 @@
 // Task 1: Obtaining String Length
-
+let message = "Hello, World!";
+console.log("Message length: " + message.length);
 
 // Task 2: Converting Cases
-
+console.log("Uppercase: " + message.toUpperCase());
+console.log("Lowercase: " + message.toLowerCase());
 
 // Task 3: Extracting Substrings
-
+let sentence = "The quick brown fox jumps over the lazy dog";
+console.log("Substring:" + sentence.substring(16,25));
 
 // Task 4: Splitting Strings
+sentence = "The quick brown fox";
+console.log("Split array: " + sentence.split(" "));
