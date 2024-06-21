edc0980d5231ddb4805721f393fc35002f9a27f9
Marko <markogidej@yahoo.com>
2024-06-21 11:57:07 -0400

    Init Commit

---
+++ arrayManipulation.js
@@ -0,0 +1,7 @@
+// Task 1: Adding and Removing Elements
+
+
+// Task 2: Sorting Arrays
+
+
+// Task 3: Applying Array Methods
---
+++ stringManipulation.js
@@ -0,0 +1,10 @@
+// Task 1: Obtaining String Length
+
+
+// Task 2: Converting Cases
+
+
+// Task 3: Extracting Substrings
+
+
+// Task 4: Splitting Strings
